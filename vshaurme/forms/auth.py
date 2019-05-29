b5300ebb95c179d6bbc677f8cbd6b31649272dfb
Odintsov Timur <timodin@yandex.ru>
2019-05-29 16:16:03 +0300

    updated RegisterForm

--- vshaurme/forms/auth.py
+++ vshaurme/forms/auth.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms import ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
+from vshaurme.validators import check_passwords_rules, check_swear_usernames
 from vshaurme.models import User
 
 
@@ -15,28 +16,20 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
-
+    
 class RegisterForm(FlaskForm):
     name = StringField('Имя', validators=[DataRequired(), Length(1, 30)])
     email = StringField('Email', validators=[DataRequired(), Length(1, 254), Email()])
-    username = StringField('Имя пользователя', validators=[DataRequired(), Length(1, 20),
-                                                   Regexp('^[a-zA-Z0-9]*$',
+    username = StringField('Имя пользователя', validators=[DataRequired(), check_swear_usernames, 
+                                                   Length(1, 20), Regexp('^[a-zA-Z0-9]*$',
                                                           message='Имя пользователя должно состоять из a-z, A-Z или 0-9.')])
     password = PasswordField('Пароль', validators=[
         DataRequired(), Length(10, 128, message='Длинна пароля должна быть не меньше 10 символов'), 
-             EqualTo('password2')])
+             EqualTo('password2'), check_passwords_rules])
     password2 = PasswordField('Подтвердите пароль', validators=[DataRequired()])
     submit = SubmitField('Присоединиться')
 
-    def validate_password(self, field):
-        if field.data.isdigit():
-            raise ValidationError('Пароль не должен состоять только из цифр')
-        elif field.data.isalpha():
-            raise ValidationError('Пароль не должен состоять только из букв')
-        elif field.data.islower():
-            raise ValidationError('Пароль не должен состоять только из букв нижнего регистра')
-        elif field.data.isupper():
-            raise ValidationError('Пароль не должен состоять только из букв верхнего регистра')
+    
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data.lower()).first():
@@ -45,9 +38,8 @@ class RegisterForm(FlaskForm):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Это имя пользователя уже используется.')
-        for word in swear_words:
-            if word in field.data.lower():
-                raise ValidationError('Имя пользователя не должно содержать бранных слов')
+
+    
 
 
 class ForgetPasswordForm(FlaskForm):
