9e0e2afe0d58023f3e6d4c8ba4a33fe68ab2b20b
Odintsov Timur <timodin@yandex.ru>
2019-06-02 19:31:07 +0300

    Update validators.py

--- vshaurme/validators.py
+++ vshaurme/validators.py
@@ -1,14 +1,14 @@
 from wtforms import ValidationError
 
 
-def check_swear_usernames(self, field):
+def check_swear_usernames(form, field):
     swear_words = ['fuck', 'shit', 'cunt', 'asshole', 'bitch', 'bint', 'xuy', 'pizda', 'ebat']
     for word in swear_words:
         if word in field.data.lower():
             raise ValidationError('Имя пользователя не должно содержать бранных слов')
 
 
-def check_passwords_rules(self, field):
+def check_passwords_rules(form, field):
         if field.data.isdigit():
             raise ValidationError('Пароль не должен состоять только из цифр')
         elif field.data.isalpha():
